36702be3346eaed9163b4dd4382e16eb3cb320cb
jizx <1822980003@qq.com>
2016-07-08 17:14:20 +0800

    eclipse项目尝试

--- Financial/src/common/Round.java
+++ Financial/src/common/Round.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 public class Round {
 	public static double round(double original){
 		BigDecimal b = new BigDecimal(original);
-		//四舍五入，保留两位小数
+		//四舍五入，保留两位小数 jzx
 		double result = b.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
 		System.out.println(result);
 		return result;		
